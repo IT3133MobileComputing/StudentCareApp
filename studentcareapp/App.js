feed65eb0e9cee4dce95498c352ea6fcd6878048
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2025-01-04 18:11:47 +0530

    calling some components for testing

--- studentcareapp/App.js
+++ studentcareapp/App.js
@@ -1,12 +1,19 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Banner from './component/Banner';
+import Login from './component/Login';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        
+        <Header/>
+        <Banner/>
+        <Login/>
+        <Footer/>
       </NavigationContainer>
     </SafeAreaProvider>
   );
