eecb69c67339e9ce3e22804202118a536c8e3389
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2025-01-04 16:45:00 +0530

    feat: finalize the footer.js component

--- studentcareapp/component/Footer.js
+++ studentcareapp/component/Footer.js
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { PaperProvider, Appbar, Divider, Button } from 'react-native-paper';
+import { StyleSheet, View, Text, ScrollView } from 'react-native';
 
 export default function Footer() {
-  return (
-    <View>
-      <Text>Footer</Text>
-    </View>
-  )
+    return (
+        <PaperProvider>
+            <ScrollView>
+                <View style={styles.footer}>
+                    <Text style={styles.footerText}>UoV © 2024</Text>
+                </View>
+            </ScrollView>
+        </PaperProvider>
+    )
 }
+const styles = StyleSheet.create({
+    footer: {
+        flex: 2,
+        backgroundColor: 'purple',
+        height: 60,
+        position: 'relative',
+        bottom: 0,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'white'
+    },
+    footerText: {
+        color: 'white', 
+    }
+});
