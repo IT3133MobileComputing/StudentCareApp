414129278aa94f9185c2a321f0cf026c7a5345e8
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2025-01-04 16:10:27 +0530

    update the header.js component

--- studentcareapp/component/Header.js
+++ studentcareapp/component/Header.js
@@ -1,11 +1,16 @@
-import { Text } from 'react-native-paper';
-import { View } from 'react-native';
-import React from 'react';
+import { PaperProvider, Appbar, Divider, Button, Text } from 'react-native-paper';
+import { StyleSheet, View, ScrollView } from 'react-native';
 
 export default function Header() {
-  return (
-    <View>
-      <Text>Header</Text>
-    </View>
-  )
+    const _goBack = () => console.log('Went back');
+    return (
+        <PaperProvider>
+            <ScrollView>
+                <Appbar.Header mode='center-aligned' style={{ backgroundColor: 'purple'}}>
+                <Appbar.BackAction onPress={_goBack} color='white'/>
+                <Appbar.Content title="UoV Student Care" titleStyle={{ color: 'white'}} />
+                </Appbar.Header>
+            </ScrollView>
+        </PaperProvider>
+    )
 }
