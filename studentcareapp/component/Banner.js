02ec6e50acd1d67b584e6b5a0b7bf3c5c2c63627
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2025-01-04 17:03:05 +0530

    feat: create and implement image banner

--- studentcareapp/component/Banner.js
+++ studentcareapp/component/Banner.js
@@ -1,10 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { PaperProvider } from 'react-native-paper';
+import { StyleSheet, View, ScrollView, Image } from 'react-native';
 
-export default function Banner() {
-  return (
-    <View>
-      <Text>Banner</Text>
-    </View>
-  )
-}
+export default function Header() {
+    const _goBack = () => console.log('Went back');
+    return (
+        <PaperProvider>
+            <ScrollView>
+                <View style={styles.imagepad}>
+                    <Image source={require('../assets/UOV.jpg')} style={styles.image}/>
+                </View>
+            </ScrollView>
+        </PaperProvider>
+    )
+}const styles = StyleSheet.create({
+    imagepad:{
+        padding: 8,
+         alignItems: 'center',
+        flex: 2,
+        marginTop: 0
+    },
+    image:{
+        width: '80%',
+        height: 100,
+    }, 
+});
