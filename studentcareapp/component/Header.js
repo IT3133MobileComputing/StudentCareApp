7a813ebfe4a2e893069a10cd26c88c65afed12cc
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2025-01-04 15:35:23 +0530

    feat: Create Home.js component and initialize it

---
+++ studentcareapp/component/Header.js
@@ -0,0 +1,11 @@
+import { Text } from 'react-native-paper';
+import { View } from 'react-native';
+import React from 'react';
+
+export default function Header() {
+  return (
+    <View>
+      <Text>Header</Text>
+    </View>
+  )
+}
