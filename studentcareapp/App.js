06bd15de480a0de61d645ff77215a0a3fca78a13
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2025-01-05 16:04:36 +0530

    feat: remove Banner component from App for streamlined layout

--- studentcareapp/App.js
+++ studentcareapp/App.js
@@ -3,17 +3,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Header from './component/Header';
 import Footer from './component/Footer';
-import Banner from './component/Banner';
 import Login from './component/Login';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Header/>
-        <Banner/>
         <Login/>
-        <Footer/>
       </NavigationContainer>
     </SafeAreaProvider>
   );
