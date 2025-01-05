18b2f6ef17c0a5aab6aab95f8b61e81b67f04803
SandeepaDilakshana <sandeepadilakshana@gmail.com>
2025-01-06 03:02:37 +0530

    feat: implement navigation stack with BottomNavigation and Login screens

--- studentcareapp/App.js
+++ studentcareapp/App.js
@@ -4,13 +4,29 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Header from './component/Header';
 import Footer from './component/Footer';
 import Login from './component/Login';
+import Course from './component/Course';
+import Profile from './component/Profile';
+import Subjects from './component/Subjects';
+import BottomNavigation from './component/BottomNavigation';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Login/>
-      </NavigationContainer>
+            <Stack.Navigator initialRouteName="Login">
+                <Stack.Screen 
+                    name="Login" 
+                    component={Login} 
+                    options={{ headerShown: false }} 
+                />
+                <Stack.Screen 
+                    name="BottomTabs" 
+                    component={BottomNavigation} 
+                    options={{ headerShown: false }} 
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
     </SafeAreaProvider>
   );
 }
